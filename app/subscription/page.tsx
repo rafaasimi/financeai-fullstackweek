7a4110ddd0a0fab8466b9710b0498acc50b1ea51
Rafael Simionato <rafaasimi@hotmail.com>
2024-11-14 13:46:51 -0300

    feat: proteção de rota para transactions e subscription

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,13 @@
+import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
 
-export default function SubscriptionPage() {
+export default async function SubscriptionPage() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
   return <Navbar />;
 }
